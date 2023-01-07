307ee12bb688b0f7e1a55ede901287c6f01b4b85
Muhammad Daniyal <muhdaniyal252@gmail.com>
2023-01-08 00:24:53 +0500

    some updates

--- classifier/predict.py
+++ classifier/predict.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 
 model = tf.keras.models.load_model('food_classifier.h5')
-img_size = 250
+img_size = 200
 input_shape = [img_size,img_size,1]
 
 with open('objects.pickle','rb') as objs:
@@ -13,8 +13,8 @@ with open('objects.pickle','rb') as objs:
 def make_prediction(path):
     img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img,(img_size,img_size))
-    img = img.reshape(*input_shape)
+    img = img.reshape(img_size,img_size,1)
+    img = img.reshape(1,img_size,img_size,1)
     img = img/255
     pred = model.predict(img)
-    return categories.get(np.argmax(pred))
-    
+    return categories.get(np.argmax(pred)) if max(pred[0]) > 0.5 else 'Can\'t identify'
